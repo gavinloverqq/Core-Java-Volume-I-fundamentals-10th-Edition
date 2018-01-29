b234651239d89a737756c07b83e026c1d262503e
kun wan <779483309@qq.com>
2018-01-29 23:10:12 +0800

    commit

--- core_java/src/chapter4/Employee.java
+++ core_java/src/chapter4/Employee.java
@@ -6,14 +6,30 @@ class Employee {
     private final String name;//一定要在构造器中初始化，确保构造器执行后必须被设值
     private double salary;
     private LocalDate hireDay;
+    private static int nextId = 1;
+    private int id = assignId();//使用函数初始化一个域
+
+    public Employee() {
+
+    }
 
     public Employee(String n, double s, int year, int month, int day) {
+        this();//调用上面的构造器，实现一个构造器中调用另一个构造器
         name = n;
         salary = s;
 //        double salary = s; //不能在构造器中定义重名局部变量，例如这个语句是无法设置salary的
         hireDay = LocalDate.of(year, month, day);
     }
 
+    private static int assignId() {
+        int r = nextId;
+        nextId++;
+        return r;
+    }
+
+    public int getId() {
+        return id;
+    }
     public String getName() {
         return name;
     }
--- core_java/src/chapter4/EmployeeTest.java
+++ core_java/src/chapter4/EmployeeTest.java
@@ -1,6 +1,24 @@
 package chapter4;
 
+import java.util.Collections;
+
 public class EmployeeTest {
+
+    public static void tripleSalary(Employee e) {
+        e.raiseSalary(200);
+    }
+
+    public static void swap(Employee x, Employee y) {
+        Employee t = x;
+        x = y;
+        y = t;
+    }
+
+//    TODO
+    public static void realSwap(Employee x, Employee y) {
+
+    }
+
     public static void main(String[] args) {
         Employee[] staff = new Employee[3];
 
@@ -15,5 +33,21 @@ public class EmployeeTest {
         for (Employee e : staff) {
             System.out.printf("name = %s, salary = %.2f\n", e.getName(), e.getSalary());
         }
+
+        Employee e = new Employee("sb", 1000, 1000, 10,10);
+        System.out.printf("before: name = %s, salary = %.2f\n", e.getName(), e.getSalary());
+        tripleSalary(e);
+        System.out.printf("after: name = %s, salary = %.2f\n", e.getName(), e.getSalary());
+
+        System.out.println("*********************************");
+        Employee e2 = new Employee("sb2", 2000, 2000, 12,20);
+        System.out.printf("before: name = %s, id = %d, salary = %.2f\n", e2.getName(), e2.getId(), e2.getSalary());
+        System.out.printf("before: name = %s, id = %d, salary = %.2f\n", e.getName(), e.getId(), e.getSalary());
+        swap(e, e2);
+        System.out.printf("after: name = %s, salary = %.2f\n", e2.getName(), e2.getSalary());
+        System.out.printf("after: name = %s, salary = %.2f\n", e.getName(), e.getSalary());
+
+
+
     }
 }
