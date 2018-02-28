7c15f81a1a6bb7168faaa3ff3ddccd8ad65b325c
macbookAir <779483309@qq.com>
2018-02-28 10:15:31 +0800

    commit

---
+++ core_java/src/chapter5/testClassSwitch/Animal.java
@@ -0,0 +1,62 @@
+package chapter5.testClassSwitch;
+
+import chapter4.testOf4_3_6.A;
+
+/**
+ * Created by wankun on 2018/2/27.
+ */
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void say() {
+        System.out.println(name + " can say");
+    }
+}
+
+class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void say() {
+        System.out.println(super.getName()+ " wang wang");
+    }
+
+    public void run() {
+        System.out.println("dog run");
+    }
+
+
+
+    public static void main(String[] args) {
+        Dog dog = new Dog("gou");
+        Animal animal = new Animal("dong wu");
+
+        Animal[] arrAnimal = new Animal[3];
+        arrAnimal[0] = dog;
+        arrAnimal[0].say();
+//        arrAnimal[0].run();//错误，arrAnimal[0] 是Animal类型的对象
+//        dog = arrAnimal[0];
+
+        arrAnimal[1] = animal;
+        arrAnimal[1].say();
+
+        Dog[] arrDogs = new Dog[3];
+        arrDogs[0] = dog;
+//        arrDogs[1] = animal;
+//        arrDogs = arrAnimal;
+        arrAnimal = arrDogs;
+        arrAnimal[0] = new Animal("cat");
+        arrAnimal[0].say();
+
+    }
+}
